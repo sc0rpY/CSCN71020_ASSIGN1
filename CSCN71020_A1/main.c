b615243fbc6a1b8c4248d0120e9c46cbb1fd22aa
Harsh Nimje <harshnimje786@gmail.com>
2024-10-01 18:38:18 -0400

    fixed the subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -45,3 +45,20 @@ void add() {
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
 }
+
+
+#
+
+void subtract() {
+    int num1, num2, result;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    result = num1 - num2;
+
+    printf("The result of %d - %d is: %d\n", num1, num2, result);
+}
